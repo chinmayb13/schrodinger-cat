56d9ca435365592959bce8bb056e20fc0d52edc6
Chinmay Barik <chinmaybarik13@gmail.com>
2022-10-09 23:25:19 +0530

    fix product array puzzle

--- internal/array/prefix_sum/product_array.go
+++ internal/array/prefix_sum/product_array.go
@@ -28,47 +28,84 @@ Output 2:
     [10, 50, 5, 50]
 */
 
+// func GetProductArray(A []int) []int {
+// 	arrLength := len(A)
+// 	var productArr []int
+// 	prefixProduct := getPrefixProduct(A)
+// 	suffixProduct := getSuffixProduct(A)
+// 	for i := range A {
+// 		//nothing on the left
+// 		if i == 0 {
+// 			productArr = append(productArr, suffixProduct[i+1])
+// 			//nothing on the right
+// 		} else if i == arrLength-1 {
+// 			productArr = append(productArr, prefixProduct[i-1])
+// 		} else {
+// 			productArr = append(productArr, prefixProduct[i-1]*suffixProduct[i+1])
+// 		}
+// 	}
+// 	return productArr
+
+// }
+
 func GetProductArray(A []int) []int {
 	arrLength := len(A)
 	var productArr []int
-	prefixProduct := getPrefixProduct(A)
-	suffixProduct := getSuffixProduct(A)
+	prefixProduct := getPrevPrefixProduct(A)
+	suffixProduct := getPostSuffixProduct(A)
 	for i := range A {
 		//nothing on the left
 		if i == 0 {
-			productArr = append(productArr, suffixProduct[i+1])
+			productArr = append(productArr, suffixProduct[i])
 			//nothing on the right
 		} else if i == arrLength-1 {
-			productArr = append(productArr, prefixProduct[i-1])
+			productArr = append(productArr, prefixProduct[i])
 		} else {
-			productArr = append(productArr, prefixProduct[i-1]*suffixProduct[i+1])
+			productArr = append(productArr, prefixProduct[i]*suffixProduct[i])
 		}
 	}
 	return productArr
-
 }
 
-func getPrefixProduct(A []int) []int {
-	var prefixProduct []int
-	for i := range A {
-		if i == 0 {
-			prefixProduct = append(prefixProduct, A[i])
-		} else {
-			prefixProduct = append(prefixProduct, prefixProduct[i-1]*A[i])
-		}
+// func getPrefixProduct(A []int) []int {
+// 	var prefixProduct []int
+// 	for i := range A {
+// 		if i == 0 {
+// 			prefixProduct = append(prefixProduct, A[i])
+// 		} else {
+// 			prefixProduct = append(prefixProduct, prefixProduct[i-1]*A[i])
+// 		}
+// 	}
+// 	return prefixProduct
+// }
+
+// func getSuffixProduct(A []int) []int {
+// 	arrLength := len(A)
+// 	suffixProduct := make([]int, arrLength)
+// 	for i := arrLength - 1; i >= 0; i-- {
+// 		if i == arrLength-1 {
+// 			suffixProduct[i] = A[i]
+// 		} else {
+// 			suffixProduct[i] = suffixProduct[i+1] * A[i]
+// 		}
+// 	}
+// 	return suffixProduct
+// }
+
+func getPrevPrefixProduct(A []int) []int {
+	prefixProduct := make([]int, len(A))
+	prefixProduct[0] = 1
+	for i := 1; i < len(A); i++ {
+		prefixProduct[i] = prefixProduct[i-1] * A[i-1]
 	}
 	return prefixProduct
 }
 
-func getSuffixProduct(A []int) []int {
-	arrLength := len(A)
-	suffixProduct := make([]int, arrLength)
-	for i := arrLength - 1; i >= 0; i-- {
-		if i == arrLength-1 {
-			suffixProduct[i] = A[i]
-		} else {
-			suffixProduct[i] = suffixProduct[i+1] * A[i]
-		}
+func getPostSuffixProduct(A []int) []int {
+	suffixProduct := make([]int, len(A))
+	suffixProduct[len(A)-1] = 1
+	for i := len(A) - 2; i >= 0; i-- {
+		suffixProduct[i] = suffixProduct[i+1] * A[i+1]
 	}
 	return suffixProduct
 }
