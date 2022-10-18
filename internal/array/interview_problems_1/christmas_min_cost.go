83bd5c843319b51121b297fa09ed228f3d1dd672
Chinmay Barik <chinmaybarik13@gmail.com>
2022-10-18 23:21:08 +0530

    add interview problems-1 hw

---
+++ internal/array/interview_problems_1/christmas_min_cost.go
@@ -0,0 +1,82 @@
+package interviewproblems1
+
+/*
+Problem Description
+You are given an array A consisting of heights of Christmas trees and an array B of the same size consisting of the cost of each of the trees (Bi is the cost of tree Ai, where 1 ≤ i ≤ size(A)), and you are supposed to choose 3 trees (let's say, indices p, q, and r), such that Ap < Aq < Ar, where p < q < r.
+The cost of these trees is Bp + Bq + Br.
+You are to choose 3 trees such that their total cost is minimum. Return that cost.
+If it is not possible to choose 3 such trees return -1.
+
+Problem Constraints
+1 <= A[i], B[i] <= 109
+3 <= size(A) = size(B) <= 3000
+
+Input Format
+First argument is an integer array A.
+Second argument is an integer array B.
+
+Output Format
+Return an integer denoting the minimum cost of choosing 3 trees whose heights are strictly in increasing order, if not possible, -1.
+
+Example Input
+Input 1:
+
+A = [1, 3, 5]
+B = [1, 2, 3]
+
+Input 2:
+A = [1, 6, 4, 2, 6, 9]
+B = [2, 5, 7, 3, 2, 7]
+
+
+Example Output
+Output 1:
+6
+
+Output 2:
+7
+
+Example Explanation
+Explanation 1:
+We can choose the trees with indices 1, 2 and 3, and the cost is 1 + 2 + 3 = 6.
+
+Explanation 2:
+We can choose the trees with indices 1, 4 and 5, and the cost is 2 + 3 + 2 = 7.
+This is the minimum cost that we can get.
+*/
+func GetMinCost(A []int, B []int) int {
+	var minCost int = 1e9 + 7
+	//iterate through middle elements
+	for i := 1; i < len(A); i++ {
+		var leftCost int = 1e9 + 7
+		//calculate left min cost
+		for l := i - 1; l >= 0; l-- {
+			if A[l] < A[i] {
+				if B[l] < leftCost {
+					leftCost = B[l]
+				}
+			}
+		}
+
+		var rightCost int = 1e9 + 7
+		//calculate right min cost
+		for r := i + 1; r < len(A); r++ {
+			if A[r] > A[i] {
+				if B[r] < rightCost {
+					rightCost = B[r]
+				}
+			}
+		}
+		cost := leftCost + rightCost + B[i]
+		//calculate overall min cost
+		if cost < minCost {
+			minCost = cost
+		}
+
+	}
+
+	if minCost == 1e9+7 {
+		return -1
+	}
+	return minCost
+}
---
+++ internal/array/interview_problems_1/left_rotations.go
@@ -0,0 +1,66 @@
+package interviewproblems1
+
+/*
+Problem Description
+Given an array of integers A and multiple values in B, which represents the number of times array A needs to be left rotated.
+Find the rotated array for each value and return the result in the from of a matrix where ith row represents the rotated array for the ith value in B.
+
+Problem Constraints
+1 <= length of both arrays <= 2000 -10^9 <= A[i] <= 10^9 0 <= B[i] <= 2000
+
+Input Format
+The first argument given is the integer array A.
+The second argument given is the integer array B.
+
+Output Format
+Return the resultant matrix.
+
+Example Input
+Input 1:
+A = [1, 2, 3, 4, 5]
+B = [2, 3]
+
+Input 2:
+A = [5, 17, 100, 11]
+B = [1]
+
+Example Output
+Output 1:
+[ [3, 4, 5, 1, 2]
+ [4, 5, 1, 2, 3] ]
+
+Output 2:
+[ [17, 100, 11, 5] ]
+
+Example Explanation
+for input 1 -> B[0] = 2 which requires 2 times left rotations
+1: [2, 3, 4, 5, 1]
+2: [3, 4, 5, 1, 2]
+
+B[1] = 3 which requires 3 times left rotation
+1: [2, 3, 4, 5, 1]
+2: [3, 4, 5, 1, 2]
+2: [4, 5, 1, 2, 4]
+*/
+func GetRotatedArray(A []int, B []int) [][]int {
+	var result [][]int
+	for i := range B {
+		localRev := reverse(A, 0, len(A)-1)
+		//get the right rotation count from left rotation count
+		rightRotationCount := len(A) - (B[i] % len(A))
+		//reverse the subarrays
+		localRev = reverse(localRev, 0, rightRotationCount-1)
+		localRev = reverse(localRev, rightRotationCount, len(A)-1)
+		result = append(result, localRev)
+	}
+	return result
+}
+
+func reverse(inp []int, start, end int) []int {
+	out := make([]int, len(inp))
+	copy(out, inp)
+	for i, j := start, end; i <= j; i, j = i+1, j-1 {
+		out[i], out[j] = inp[j], inp[i]
+	}
+	return out
+}
