74ba27f4650912fe5c7ccad1282e96aef0439995
Chinmay Barik <chinmaybarik13@gmail.com>
2023-01-30 20:20:22 +0530

    add comments

--- internal/advanced/binary_search_2/kth_smallest.go
+++ internal/advanced/binary_search_2/kth_smallest.go
@@ -5,6 +5,13 @@ import (
 )
 
 /*
+Problem Description
+Given an integer array A of size N.
+
+If we store the sum of each triplet of the array A in a new list, then find the Bth smallest element among the list.
+
+NOTE: A triplet consists of three elements from the array. Let's say if A[i], A[j], A[k] are the elements of the triplet then i < j < k.
+
 Problem Constraints
 3 <= N <= 500
 1 <= A[i] <= 108
